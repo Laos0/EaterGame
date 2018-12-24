163192c57a67726600e5d85272a7a8f77e64926f
Laos0 <isonylao@gmail.com>
2018-12-24 10:37:03 -0600

    Almost completed.

--- EaterGame/Assets/scripts/GamesManager.cs
+++ EaterGame/Assets/scripts/GamesManager.cs
@@ -2,32 +2,48 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : Singleton<GameManager>
+public class GamesManager : Singleton<GamesManager>
 {
     public GameObject player;
     public Spawner spawner;
     private bool isGameOver;
-    private int score;
+    public int score;
     // Start is called before the first frame update
     void Start()
     {
         isGameOver = false;
         score = 0; // score that will be used to display the score 
-        InvokeRepeating("startSpawner", 2, 5);
-
+        InvokeRepeating("startSpawner", 1, 1);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(isGameOver == true)
-        {
-            // end the game, pause etc.
-        }
+       
     }
 
     void startSpawner()
     {
         spawner.spawn();
+    }
+
+    void onCollisionEnter(Collision col)
+    {
+      
+    }
+
+    public void addScore(int value)
+    {
+        score += value;
+        if (score <= 0)
+        {
+            score = 0;
+        }
+       
+    }
+
+    public int getScore()
+    {
+        return score;
     }
 }
---
+++ EaterGame/Assets/scripts/Item.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    public enum ItemType {GOOD_FOOD, BAD_FOOD, WEAPON};
+    public ItemType currentItemType;
+    private int value;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(currentItemType == ItemType.GOOD_FOOD)
+        {
+            value = 1;
+        }else if (currentItemType == ItemType.BAD_FOOD)
+        {
+            value = -1;
+        }
+        else
+        {
+            value = -2;
+        }
+    }
+
+    public int getValue()
+    {
+        return value;
+    }
+}
